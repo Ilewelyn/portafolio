b354224f0e91a55498a809b99e993731a1233999
llewelyn <inthecloud275@gmail.com>
2026-09-24 15:34:20 -0600

    Ejercicio de recursividad

---
+++ datos/unidad1/recursividad/Recursividad.java
@@ -0,0 +1,17 @@
+package datos.unidad1.recursividad;
+
+public class Recursividad {
+
+    public static void saludo(int total) {
+        if (total <= 0) {
+            return;
+        } else {
+            System.out.println("Hola");
+            saludo(total - 1);
+        }
+    }
+
+    public static void main(String[] a) {
+        saludo(10);
+    }
+}
