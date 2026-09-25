56527817491ce1de1a89229e6e2887614dd2d667
llewelyn <inthecloud275@gmail.com>
2026-09-25 14:46:20 -0600

    Agrega suma recursiva de arreglo

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -20,8 +20,27 @@ public class Recursividad {
         }
     }
 
+
+
+    public static int sumaRecursividad(int[] datos, int longitud) {
+		// si ya no quedan elementos por sumar, la suma es 0
+        if (longitud <= 0) {
+            return 0;
+        } else {
+ 		// Toma el elemento en la posición actual 
+        	// y le suma el resultado de la llamada recursiva con un elemento menos
+		return datos[longitud - 1] + sumaRecursividad(datos, longitud - 1);
+            
+        }
+    }
+
     public static void main(String[] a) {
-        saludo(10, "Ale");
-        cuentaRegresiva(10);
+        //saludo(10, "Ale");
+        //cuentaRegresiva(10);
+
+        int[] numeros = {15,6};
+
+        System.out.println("Suma: " + sumaRecursividad(numeros, numeros.length));
     }
 }
+
